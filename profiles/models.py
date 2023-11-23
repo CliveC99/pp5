cfd15670980fa1530decb11d33b57a50d252aa27
CliveC99 <cliveconnell@gmail.com>
2023-11-23 00:51:03 +0000

    Updated account templates and added my profile section

--- profiles/models.py
+++ profiles/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from django_countries.fields import CountryField
 
 
-class UserProfiles(models.Model):
+class UserProfile(models.Model):
     """
-    User profile for delivery information and order history
+    Profile model for delivery info and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -21,13 +22,13 @@ class UserProfiles(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update the user profile
+    Create or update a profile for a user
     """
     if created:
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
-
