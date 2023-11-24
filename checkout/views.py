125470d569852b0c0816309eccfdb111a0d522a8
CliveC99 <cliveconnell@gmail.com>
2023-11-24 12:15:09 +0000

    Added stripe requirements and styled the card section

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O2r6OGdP1AcvpQtklyNX62v9INUosIWD4Oa01gWJjXYCOVjt8QI53CHjmHUr7FD13MxBT3zVbKnnkOp0mcWZWU8007tnBUxZb',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
