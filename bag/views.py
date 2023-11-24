497639bdda3619a616b4ef3f93bc18a2d416eff7
CliveC99 <cliveconnell@gmail.com>
2023-11-24 09:38:01 +0000

    Add quantity updating in cart

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 
@@ -24,3 +24,19 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+def update_bag(request, item_id):
+    """
+    Updates the quantity in the bag
+    """
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+    if quantity:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            del bag[item_id]
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
