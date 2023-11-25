ac70d54767a3fffd51177fdb3e631a4299b449d0
CliveC99 <cliveconnell@gmail.com>
2023-11-25 17:39:50 +0000

    Add toast messages for update bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     Add  a quantity of products to the bag
     """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -33,15 +33,19 @@ def update_bag(request, item_id):
     """
     Updates the quantity in the bag
     """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
     if quantity:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             del bag[item_id]
+            messages.success(request, f'Removed {product.name} from your bag')
     else:
         bag.pop(item_id)
+
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
