55e0a63859fef8ca84a169611ed5a9eaf8d07ec1
CliveC99 <cliveconnell@gmail.com>
2023-11-25 17:24:28 +0000

    Add toast messages for add to bag

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -12,14 +13,18 @@ def add_to_bag(request, item_id):
     """
     Add  a quantity of products to the bag
     """
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
