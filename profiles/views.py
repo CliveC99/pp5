851ebf0dc149990a8441cd756266ae32e695a716
CliveC99 <cliveconnell@gmail.com>
2023-11-26 01:49:52 +0000

    Continue to work on order history/saving details

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
-
+from checkout.models import Order
 
 @login_required
 def profile(request):
@@ -25,6 +25,20 @@ def profile(request):
     context = {
         'form': form,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_history):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'This is an old conformation for {order_number}. An email was sent on the order date.')
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
