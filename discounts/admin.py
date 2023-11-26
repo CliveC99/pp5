0db6cbb77856c2388d86befbd9a772877ba2c860
CliveC99 <cliveconnell@gmail.com>
2023-11-26 21:15:29 +0000

    Remove discounts

--- discounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Discounts
-
-
-class DiscountsAdmin(admin.ModelAdmin):
-    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
-
-admin.site.register(Discounts, DiscountsAdmin)
--- fkperformance/settings.py
+++ fkperformance/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'profiles',
     'bag',
     'checkout',
-    'discounts',
 ]
 
 MIDDLEWARE = [
