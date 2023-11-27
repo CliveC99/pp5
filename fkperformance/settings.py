4c6f732dbe1e0221973b57fb861bb6512bd47be6
CliveC99 <>
2023-11-27 16:12:55 +0000

    Update allowed hosts

--- fkperformance/settings.py
+++ fkperformance/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-92i3c$!0r+4as6ocxp_3wuxnp0uk$j0yw53tt!fyylal1x8(5o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8001-clivec99-pp5-yvom3hp9pv.us2.codeanyapp.com', 'pp5-fk-ff4c1b683208.herokuapp.com']
+ALLOWED_HOSTS = ['8001-clivec99-pp5-yvom3hp9pv.us2.codeanyapp.com', 'pp5-fk.herokuapp.com', 'pp5-fk-ff4c1b683208.herokuapp.com']
 
 
 # Application definition
