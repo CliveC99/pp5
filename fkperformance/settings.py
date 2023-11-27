e240e188c72ab0eecf5f3067402a086771cb2a76
CliveC99 <>
2023-11-27 16:30:23 +0000

    Try fix deployment issue

--- fkperformance/settings.py
+++ fkperformance/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'fkperformance.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-if 'DATABASE_URL' in os.enviorn:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
