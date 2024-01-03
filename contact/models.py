bf734f3577a08f7eb933748b6622b60166832c4c
CliveC99 <>
2024-01-03 00:22:44 +0000

    Finish contact functionality

--- contact/models.py
+++ contact/models.py
@@ -7,6 +7,3 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.full_name
-
-    def get_friendly_name(self):
-        return self.email
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
-    contact_form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            contact = form.save()
+            messages.success(request, 'Successfully Contacted!')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(request,
+                           ('Failed to contact the page, try again!'))
+    else:
+        contact_form = ContactForm()
+
     template = 'contact/contact.html'
     context = {
         'contact_form': contact_form,
