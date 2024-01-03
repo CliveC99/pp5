75298281d479ef24600326720cbe81ec4d5a2161
CliveC99 <>
2024-01-03 02:01:11 +0000

    Get newsletter page to display

--- fkperformance/urls.py
+++ fkperformance/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('contact/', include('contact.urls')),
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'fkperformance.views.handler404'
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,1 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import NewsletterForm
+from django.contrib import messages
+
+
+def newsletter(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST, request.FILES)
+        if form.is_valid():
+            newsletter = form.save()
+            messages.success(request, 'Thanks for subscribing!')
+            return redirect(reverse('newsletter'))
+        else:
+            messages.error(request,
+                           ('Failed to subscribe to the newsletter, try again!'))
+    else:
+        newsletter_form = NewsletterForm()
+
+    template = 'newsletter/newsletter.html'
+    context = {
+        'newsletter_form': newsletter_form,
+    }
+
+    return render(request, template, context)
