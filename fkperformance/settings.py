1cb0e3bdb4561b3c783b7b05aad1f8073fc6f09f
CliveC99 <>
2024-01-03 00:34:37 +0000

    Add a contact us buttons and style buttons

--- fkperformance/settings.py
+++ fkperformance/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-clivec99-pp5-mr9tnbf08s.us2.codeanyapp.com', 'pp5-fk.herokuapp.com', 'pp5-fk-ff4c1b683208.herokuapp.com']
 
