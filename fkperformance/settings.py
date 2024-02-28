c43106c34deba1e756eb5d897e3b1d7e76b6c115
CliveC99 <cliveconnell@gmail.com>
2024-02-28 03:37:57 +0000

    Add font awesome icons to contact page

--- fkperformance/settings.py
+++ fkperformance/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-clivec99-pp5-mr9tnbf08s.us2.codeanyapp.com', 'pp5-fk.herokuapp.com', 'pp5-fk-ff4c1b683208.herokuapp.com']
+ALLOWED_HOSTS = ['8000-clivec99-pp5-95lx51wk4xz.ws-eu108.gitpod.io', 'pp5-fk.herokuapp.com', 'pp5-fk-ff4c1b683208.herokuapp.com']
 
 
 # Application definition
