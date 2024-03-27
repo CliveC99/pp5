a8fd1ef36e09581be4e96a522476a1068f401b02
CliveC99 <cliveconnell@gmail.com>
2024-03-27 02:37:16 +0000

    Fix typo in views.py for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -131,12 +131,6 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        order.user_profile = profile
-        order.save()
-
-
         if save_info:
             profile_data = {
                 'default_phone_number': order.phone_number,
